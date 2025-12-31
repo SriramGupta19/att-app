0776c17bd7758db03c205807257d638ec36267ce
Sri Ram Gupta <76065013+SriramGupta19@users.noreply.github.com>
2025-12-31 17:37:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /* script.js */
-// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDoc, doc, updateDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDoc, getDocs, doc, updateDoc, query, where, orderBy, Timestamp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// --- CONFIGURATION START ---
-// COPY THIS FROM YOUR FIREBASE CONSOLE
+// --- PASTE YOUR FIREBASE CONFIG HERE ---
 const firebaseConfig = {
   apiKey: "AIzaSyDcOx7keARxAM-W3aLoUzO1gDdfwsmp-PM",
   authDomain: "attendance-app-33e78.firebaseapp.com",
@@ -13,17 +11,15 @@ const firebaseConfig = {
   messagingSenderId: "947436572816",
   appId: "1:947436572816:web:d98f3b04ed7b32f4b14815"
 };
-// --- CONFIGURATION END ---
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export functions to be used by HTML files
-export { db, collection, addDoc, getDoc, doc, updateDoc, Timestamp };
+export { db, collection, addDoc, getDoc, getDocs, doc, updateDoc, query, where, orderBy, Timestamp };
 
-// --- UTILITY: HAVERSINE FORMULA (Calculate Distance) ---
+// --- UTILITY: HAVERSINE FORMULA ---
 export function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the earth in km
+    const R = 6371; 
     const dLat = deg2rad(lat2 - lat1);
     const dLon = deg2rad(lon2 - lon1);
     const a =
@@ -31,10 +27,8 @@ export function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
         Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
         Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c; // Distance in km
-    return d * 1000; // Return in meters
+    const d = R * c; 
+    return d * 1000; 
 }
 
-function deg2rad(deg) {
-    return deg * (Math.PI / 180);
-}
+function deg2rad(deg) { return deg * (Math.PI / 180); }
